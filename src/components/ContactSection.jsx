0f6f14f4b109ab44044b6856d84694aaa0a3209e
lokesh0221 <venkatlokesh.221@gmail.com>
2025-11-07 22:00:41 +0530

    updated

--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -93,17 +93,12 @@ export const ContactSection = () => {
             <div className="pt-8">
               <h4 className="font-medium mb-4"> Connect With Me</h4>
               <div className="flex space-x-4 justify-center">
-                <a href="#" target="_blank">
+                <a
+                  href="https://www.linkedin.com/in/venkatlokesh221/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
                   <Linkedin />
-                </a>
-                <a href="#" target="_blank">
-                  <Twitter />
-                </a>
-                <a href="#" target="_blank">
-                  <Instagram />
-                </a>
-                <a href="#" target="_blank">
-                  <Twitch />
                 </a>
               </div>
             </div>
